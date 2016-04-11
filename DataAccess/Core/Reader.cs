28b07861b9430b1eb5bac5e877a06c6cc4e5f416
Aleksandr Gorshukov <Aleksandr_Gorshukov@epam.com>
2016-04-11 11:44:08 +0000

    add reader

---
+++ DataAccess/Core/Reader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public abstract class Reader<T>
+        where T : class
+    {
+        public abstract List<T> LoadList(DbDataReader reader);
+    }
+}
---
+++ DataAccess/Core/SimpleReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class SimpleReader<T> : Reader<T>
+        where T : class
+    {
+        private readonly Func<DbDataReader, T> entityReader;
+        public SimpleReader(Func<DbDataReader, T> reader)
+        {
+            entityReader = reader;
+        }
+        public override List<T> LoadList(DbDataReader reader)
+        {
+            var result = new List<T>();
+
+            if (reader!=null)
+            {
+                while (reader.Read())
+                    result.Add(entityReader(reader));
+            }
+
+            return result;
+        }
+    }
+}
